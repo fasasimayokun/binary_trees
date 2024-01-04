fa87ff0f52e90fd67f7c48e57d1a1e384fc9dfe4
fasasimayokun <maykay.fash@gmail.com>
2024-01-04 06:27:57 -0800

    my commit for 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t lft = 0, rgt = 0;
-		
+
 		lft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		rgt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
